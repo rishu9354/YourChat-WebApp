5b1cd1dfcc78580a114dc62cb23ba67be283133d
rishu9354 <rs682688@gmail.com>
2025-01-23 15:55:03 +0530

    deploy

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,85 @@
 <template>
-  <div class="view login">
-    <form action="" class="login-form">
+  <div class="view login" v-if="state.username === '' || state.username === null">
+    <form action="" class="login-form" v-on:submit.prevent="login"> 
       <div class="form-inner">
         <h1>Login to YourChat</h1>
       <label for="username">Username</label>
-      <input type="text" placeholder="Enter your username.."/>
+      <input type="text" placeholder="Enter your username.." v-model="inputUsername"/>
       <input type="submit" value="Login">
       </div>
     </form>
   </div>
   
-  <div class="view chat">
-    <h1>Chat view</h1>
+  <div class="view chat" v-else>
+    <header>
+      <button class="logout" @click="logOut">Logout</button>
+      <h1>Welcome, {{ state.username }}</h1>
+    </header>
+
+    <section class="chat-box">
+      <!-- chatting -->
+       //message
+    </section>
+
+    <footer>
+      <form @submit.prevent="sendMessage">
+        <input type="text" placeholder="Write a message..." v-model="inputMessage">
+        <input type="submit" value="send" />
+      </form>
+    </footer>
   </div>
 </template>
 
 <script>
-// import db from './db';
-
+import db from './db.js';
+import { reactive, onMounted, ref } from 'vue';
 export default {
   name: 'App',
+  setup (){
+    const inputUsername =ref("");
+    const inputMessage =ref("");
+
+    const state = reactive({
+      username:"",
+      messages:[]
+    })
+    const login = ()=>{
+      if(inputUsername.value !="" || inputUsername.value != null){
+        state.username = inputUsername.value;
+        inputUsername.value = "";
+        // console.log(username);
+        
+      }
+    }
+    const sendMessage =()=>{
+      const messageRef = db.database().ref("messages");
+      console.log('testing1');
+      
+      if(inputMessage.value === "" || inputMessage.value === null){
+        return;
+      }
+      console.log('testing2');
+      
+      const message ={
+        username : state.username,
+        content: inputMessage.value
+      }
+// ppush the value into database
+      messageRef.push(message);
+      inputMessage.value="";
+    }
+    const logOut =()=>{
+      state.username="";
+    }
+    return{
+      inputUsername,
+      login,
+      state,
+      logOut,
+      inputMessage,
+      sendMessage
+    }
+  }
 
 }
 </script>
--- src/db.js
+++ src/db.js
@@ -1,21 +1,18 @@
 import firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
+
 import "firebase/database";
 
 const config = {
-    //   api keys get from firebase websites
-    apiKey: "AIzaSyBTJpDtDHAsToZ6JxlWrEk7h02MgYh5pvo",
-
-    authDomain: "chat-webapp-5f636.firebaseapp.com",
-
-    projectId: "chat-webapp-5f636",
-
-    storageBucket: "chat-webapp-5f636.firebasestorage.app",
-
-    messagingSenderId: "1064979605679",
-
-    appId: "1:1064979605679:web:45511cb2729bea704ecc5a"
-
+  //   api keys get from firebase websites
+  apiKey: "AIzaSyBYUECBFprGLfCTvWYVAnBmQq2aIG2GkaE",
+  authDomain: "chat-webapp-2.firebaseapp.com",
+  databaseURL: "https://chat-webapp-2-default-rtdb.firebaseio.com",
+  projectId: "chat-webapp-2",
+  storageBucket: "chat-webapp-2.firebasestorage.app",
+  messagingSenderId: "386373855971",
+  appId: "1:386373855971:web:30d150555b0ea4e5f4f3d6"
 };
 
-const db = firebase.initializeApp(config);
+const db = initializeApp(config);
 export default db;
